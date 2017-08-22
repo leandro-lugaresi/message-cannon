fc8c8c451ff9eca48a99d74158f7653c7db832cf
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2017-08-22 01:36:25 -0300

    New cosumer finished

--- cmd/consumer.go
+++ cmd/consumer.go
@@ -19,19 +19,35 @@ func newConsumer(cnf config.ConsumerConfig, con *amqp.Connection) (*consumer, er
 	if nil != err {
 		return nil, fmt.Errorf("Failed to open a RabbitMQ channel: %s", err.Error())
 	}
-	log.Info().Msg("Channel opened")
-	log.Info().Msg("Setting QoS")
-	if err := ch.Qos(cnf.PrefetchCount, cnf.PrefetchSize, false); err != nil {
-		return nil, fmt.Errorf("Failed to set QoS: %s", err.Error())
+	if len(cnf.Exchange.Name) > 0 {
+		log.Info().Msgf("Declaring queue \"%s\"", cnf.Exchange.Name)
+		ch.ExchangeDeclare(
+			cnf.Exchange.Name,
+			cnf.Exchange.Type,
+			cnf.Exchange.Options.Durable,
+			cnf.Exchange.Options.AutoDelete,
+			cnf.Exchange.Options.Internal,
+			cnf.Exchange.Options.NoWait,
+			cnf.Exchange.Options.Args)
 	}
-	log.Info().Msg("Succeeded setting QoS")
 	log.Info().Msgf("Declaring queue \"%s\"", cnf.Queue.Name)
-	_, err = ch.QueueDeclare(
+	q, err := ch.QueueDeclare(
 		cnf.Queue.Name,
 		cnf.Queue.Options.Durable,
 		cnf.Queue.Options.AutoDelete,
 		cnf.Queue.Options.Exclusive,
 		cnf.Queue.Options.NoWait,
 		cnf.Queue.Options.Args)
-
+	if err != nil {
+		return nil, err
+	}
+	log.Info().Msg("Adding queue binds")
+	for _, k := range cnf.Queue.RoutingKeys {
+		ch.QueueBind(q.Name, k, cnf.Exchange.Name, cnf.Queue.BindingOptions.NoWait, cnf.Queue.BindingOptions.Args)
+	}
+	log.Info().Msg("Setting QoS")
+	if err := ch.Qos(cnf.PrefetchCount, cnf.PrefetchSize, false); err != nil {
+		return nil, fmt.Errorf("Failed to set QoS: %s", err.Error())
+	}
+	return &consumer{channel: ch, config: cnf}, nil
 }
